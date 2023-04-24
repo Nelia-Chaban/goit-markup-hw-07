9d6ffeb0ee5df0fbdd129051b3a2807ceabca352
Nelia-Chaban <126400490+Nelia-Chaban@users.noreply.github.com>
2023-04-25 00:04:48 +0300

    Update html css

--- js/modal.js
+++ js/modal.js
@@ -1,16 +1,15 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelectorAll("[data-modal-open]"),
+    openModalBtn: document.querySelector("[data-modal-open]"),
     closeModalBtn: document.querySelector("[data-modal-close]"),
     modal: document.querySelector("[data-modal]"),
   };
 
-  refs.openModalBtn.forEach((element) => {
-    element.addEventListener("click", toggleModal);
-  });
-
+  refs.openModalBtn.addEventListener("click", toggleModal);
   refs.closeModalBtn.addEventListener("click", toggleModal);
-
+  refs.modal.addEventListener("click", (click) => {
+    if (click.target === click.currentTarget) toggleModal();
+  });
   function toggleModal() {
     refs.modal.classList.toggle("is-hidden");
     document.body.classList.toggle("no-scroll");
